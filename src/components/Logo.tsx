e912f88a148047fe5cde08bf67a2afa5e1bda5c4
Brian Li <brnli7@gmail.com>
2021-04-04 18:31:17 -0700

    Remove fake logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Flex, Text } from "theme-ui";
 import { Link } from "react-router-dom";
-import { LogoIcon } from "icons/LogoIcon";
 
 export const Logo = () => {
   return (
     <Link to="/" style={{ textDecoration: "none", color: "black" }}>
       <Flex sx={{ alignItems: "center" }}>
-        <LogoIcon />
         <Text sx={{ ml: 1 }} variant="logo">
           poof
         </Text>
