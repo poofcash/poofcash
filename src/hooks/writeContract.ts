bb630149c829ba9da29529b3bc6f6bbaa6fa0c5f
Brian Li <brnli7@gmail.com>
2021-07-02 14:12:48 -0700

    Support v1 and v2

--- src/hooks/writeContract.ts
+++ src/hooks/writeContract.ts
@@ -1,39 +1,38 @@
 import React from "react";
 import { ContractKit } from "@celo/contractkit";
-import { MaxUint256 } from "@ethersproject/constants";
 import { useContractKit } from "@ubeswap/use-contractkit";
 import { toWei } from "web3-utils";
 import { PoofKitV2 } from "@poofcash/poof-kit";
-import { Address } from "@celo/base";
 import { CHAIN_ID } from "config";
 import { PoofKitGlobal } from "./poofUtils";
 import { useAsyncState } from "./useAsyncState";
 
 export function useApprove(
-  tokenAddress: Address,
-  amountToApprove: string
+  currency: string,
+  amount: string
 ): [string, () => Promise<void>, boolean] {
   const { address, performActions } = useContractKit();
   const { poofKit } = PoofKitGlobal.useContainer();
   const [loading, setLoading] = React.useState(false);
 
   const allowanceCall = React.useCallback(async () => {
-    if (!address || !tokenAddress) {
+    if (!address) {
       return "0";
     }
-    return await poofKit.allowance(tokenAddress, address);
-  }, [poofKit, address, tokenAddress]);
+    return await poofKit.allowance(CHAIN_ID, currency, amount, address);
+  }, [poofKit, address, currency, amount]);
   const [allowance, refetchAllowance] = useAsyncState("0", allowanceCall);
 
   const approve = React.useCallback(async (): Promise<void> => {
-    const useExact = true;
     setLoading(true);
     try {
       await performActions(async (kit: ContractKit) => {
         const poofKit = new PoofKitV2(kit, CHAIN_ID);
         const approveTxo = poofKit.approve(
-          tokenAddress,
-          useExact ? amountToApprove : MaxUint256.toString()
+          CHAIN_ID,
+          currency,
+          amount
+          // TODO: useExact ? amount : MaxUint256.toString()
         );
         const tx = await kit.sendTransactionObject(approveTxo, {
           from: kit.defaultAccount,
@@ -47,7 +46,7 @@ export function useApprove(
     } finally {
       setLoading(false);
     }
-  }, [tokenAddress, amountToApprove, performActions, refetchAllowance]);
+  }, [currency, amount, performActions, refetchAllowance]);
 
   return [allowance, approve, loading];
 }
