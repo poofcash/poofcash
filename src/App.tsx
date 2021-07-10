aea19c69f9c863cb4e10147be2f0481dbf85d061
Brian Li <brnli7@gmail.com>
2021-07-10 07:35:30 -0700

    Remove unmounting on page change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import CompliancePage from "pages/CompliancePage";
 import WithdrawPage from "pages/WithdrawPage";
 import DepositPage from "pages/DepositPage";
-import { Container, Flex, Link, Text } from "theme-ui";
+import { Box, Container } from "theme-ui";
 import { Header } from "components/Header";
 import "i18n/config";
 import { useSelector } from "react-redux";
@@ -14,8 +14,6 @@ import RedeemPage from "pages/RedeemPage";
 import { SetupAccount } from "pages/SetupAccount";
 import AirdropPage from "pages/AirdropPage";
 import ExchangePage from "pages/ExchangePage";
-import { CHAIN_ID, MINE_START } from "config";
-import { Countdown } from "components/Countdown";
 import Modal from "react-modal";
 
 // pass props and State interface to Component class
@@ -29,55 +27,6 @@ const App: React.FC = () => {
     (state: AppState) => state.global.currentPage
   );
 
-  let page = <DepositPage />;
-  if (currentPage === Page.WITHDRAW) {
-    page = <WithdrawPage />;
-  } else if (currentPage === Page.COMPLIANCE) {
-    page = <CompliancePage />;
-  } else if (currentPage === Page.MINE) {
-    page = <MinePage />;
-  } else if (currentPage === Page.REDEEM) {
-    page = <RedeemPage />;
-  } else if (currentPage === Page.SETUP) {
-    page = <SetupAccount />;
-  } else if (currentPage === Page.AIRDROP) {
-    page = <AirdropPage />;
-  } else if (currentPage === Page.EXCHANGE) {
-    page = <ExchangePage />;
-  }
-
-  const currentTime = Math.round(Date.now() / 1000);
-  if (CHAIN_ID === 42220 && currentTime < MINE_START) {
-    if (
-      currentPage === Page.MINE ||
-      currentPage === Page.REDEEM ||
-      currentPage === Page.AIRDROP
-    ) {
-      let title = "Mining";
-      if (currentPage === Page.REDEEM) {
-        title = "Redemption";
-      } else if (currentPage === Page.AIRDROP) {
-        title = "Airdrop";
-      }
-      page = (
-        <Flex sx={{ flexDirection: "column", alignItems: "center" }}>
-          <Text variant="title">{title} begins in</Text>
-          <Countdown start={MINE_START - 1209600} end={MINE_START} />
-          <Text mt={4}>
-            Tune in to updates on{" "}
-            <Link
-              href="https://twitter.com/Poofcash"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </Link>
-          </Text>
-        </Flex>
-      );
-    }
-  }
-
   return (
     <>
       <Container
@@ -88,23 +37,74 @@ const App: React.FC = () => {
           width: "auto",
         }}
       >
-        {currentPage === Page.SETUP ? (
-          <SetupAccount />
-        ) : (
-          <>
-            <Header />
-            <Container
-              sx={{
-                px: [3, 0],
-                py: [4, 0],
-                mb: "64px",
-                maxHeight: "calc(100vh + 64px)",
-              }}
-            >
-              {page}
-            </Container>
-          </>
-        )}
+        <Box
+          sx={{
+            display: currentPage === Page.SETUP ? "none" : "inherit",
+          }}
+        >
+          <Header />
+        </Box>
+        <Container
+          sx={{
+            px: [3, 0],
+            py: [4, 0],
+            mb: "64px",
+            maxHeight: "calc(100vh + 64px)",
+          }}
+        >
+          <Box
+            sx={{
+              display: currentPage === Page.DEPOSIT ? "inherit" : "none",
+            }}
+          >
+            <DepositPage />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.WITHDRAW ? "inherit" : "none",
+            }}
+          >
+            <WithdrawPage />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.COMPLIANCE ? "inherit" : "none",
+            }}
+          >
+            <CompliancePage />
+          </Box>
+          <Box sx={{ display: currentPage === Page.MINE ? "inherit" : "none" }}>
+            <MinePage />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.REDEEM ? "inherit" : "none",
+            }}
+          >
+            <RedeemPage />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.SETUP ? "inherit" : "none",
+            }}
+          >
+            <SetupAccount />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.AIRDROP ? "inherit" : "none",
+            }}
+          >
+            <AirdropPage />
+          </Box>
+          <Box
+            sx={{
+              display: currentPage === Page.EXCHANGE ? "inherit" : "none",
+            }}
+          >
+            <ExchangePage />
+          </Box>
+        </Container>
         {/*TODO footer*/}
       </Container>
       {passwordModal}
