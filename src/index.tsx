ef412d6cc75b26e4539ef95b7943b70e285304c6
Brian Li <brnli7@gmail.com>
2021-07-25 17:36:42 -0700

    Add serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,8 @@ import { ContractKitProvider } from "@celo-tools/use-contractkit";
 import { PoofAccountGlobal } from "hooks/poofAccount";
 import { PoofKitGlobal } from "hooks/usePoofKit";
 import { DepositListGlobal } from "components/DepositList";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 import "@celo-tools/use-contractkit/lib/styles.css";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -47,3 +49,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
